b9856f89694fb458768aad18a74c51cf468abd18
FiagoTagundes <98733763+FiagoTagundes@users.noreply.github.com>
2023-01-06 07:36:22 -0300

    Design inicial do Alura Space

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import "./styles/estilosGlobais.scss";
+import PaginaInicial from "./Paginas/paginaInicial";
+
+export default function App() {
+  return <PaginaInicial />;
+}
---
+++ src/Paginas/paginaInicial/index.jsx
@@ -0,0 +1,29 @@
+import banner from "./banner.png";
+import Cabecalho from "../../Componentes/Cabecalho";
+import Menu from "../../Componentes/Menu";
+import styles from "./PaginaInicial.module.scss";
+import Rodape from "../../Componentes/Rodape";
+import Galeria from "../../Componentes/Galeria";
+import Populares from "../../Componentes/Populares";
+
+export default function PaginaInicial() {
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
+        </div>
+      </main>
+      <Rodape />
+    </>
+  );
+}
